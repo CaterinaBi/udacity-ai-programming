049ff70daeae80d81a6aa5f559880adb0607f04a
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-04-08 10:07:28 +0100

    Final version

--- program/adjust_results4_isadog.py
+++ program/adjust_results4_isadog.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: CaterinaBi
 # DATE CREATED: 06.04.2022                              
-# REVISED DATE: 
+# REVISED DATE: 08.04. 2022
 # PURPOSE: Create a function adjust_results4_isadog that adjusts the results 
 #          dictionary to indicate whether or not the pet image label is of-a-dog, 
 #          and to indicate whether or not the classifier image label is of-a-dog.
@@ -97,8 +97,6 @@ def adjust_results4_isadog(results_dic, dogfile):
     # two items to end of value(List) in results_dic. 
     # List Index 3 = whether(1) or not(0) Pet Image Label is a dog AND 
     # List Index 4 = whether(1) or not(0) Classifier Label is a dog
-    # How - iterate through results_dic if labels are found in dognames_dic
-    # then label "is a dog" index3/4=1 otherwise index3/4=0 "not a dog"
     for key in results_dic:
 
         # Pet Image Label IS of Dog (e.g. found in dognames_dic)
