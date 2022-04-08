078ed5febd47c1f813415f7b3f45a2d155be54b8
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-04-08 10:08:37 +0100

    Final version

--- program/calculates_results_stats.py
+++ program/calculates_results_stats.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: CaterinaBi
 # DATE CREATED: 06.04.2022                                
-# REVISED DATE: 
+# REVISED DATE: 08.04.2022
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
 #          architecture to classify the images. This function will use the 
