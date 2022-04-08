48e628b64a383eaffff64daf6d8174fb74094800
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-04-08 10:21:02 +0100

    Final version

--- program/print_results.py
+++ program/print_results.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: CaterinaBi
 # DATE CREATED: 07.04.2022
-# REVISED DATE: 
+# REVISED DATE: 08.04.2022
 # PURPOSE: Create a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
 #          should also allow the user to be able to print out cases of misclassified
@@ -26,11 +26,6 @@
 #         This function does not output anything other than printing a summary
 #         of the final results.
 ##
-# TODO 6: Define print_results function below, specifically replace the None
-#       below by the function definition of the print_results function. 
-#       Notice that this function doesn't to return anything because it  
-#       prints a summary of the results using results_dic and results_stats_dic
-# 
 def print_results(results_dic, results_stats_dic, model, 
                   print_incorrect_dogs = False, print_incorrect_breed = False):
     """
@@ -67,30 +62,13 @@ def print_results(results_dic, results_stats_dic, model,
           "***")
     print("{}: {}".format('N Images', results_stats_dic['n_images']))
     print("{}: {}".format('N Dog Images', results_stats_dic['n_dogs_img']))
-
-    # TODO: 6a. REPLACE print("") with CODE that prints the text string 
-    #          'N Not-Dog Images' and then the number of NOT-dog images 
-    #          that's accessed by key 'n_notdogs_img' using dictionary 
-    #          results_stats_dic
-    #
     print("{}: {}".format('N Not-Dog Images', results_stats_dic['n_notdogs_img']))
-
 
     # Prints summary statistics (percentages) on Model Run
     print("\nSummary statistics on Model Run:")
     for key in results_stats_dic:
-        # TODO: 6b. REPLACE pass with CODE that prints out all the percentages 
-        #           in the results_stats_dic dictionary. Recall that all 
-        #           percentages in results_stats_dic have 'keys' that start with 
-        #           the letter p. You will need to write a conditional 
-        #           statement that determines if the key starts with the letter
-        #           'p' and then you want to use a print statement to print 
-        #           both the key and the value. Remember the value is accessed 
-        #           by results_stats_dic[key]
-        #
         if key[0] == 'p':
             print("{}: {}".format(key, results_stats_dic[key]))
-
 
     # IF print_incorrect_dogs == True AND there were images incorrectly 
     # classified as dogs or vice versa - print out these cases
@@ -102,23 +80,6 @@ def print_results(results_dic, results_stats_dic, model,
 
         # process through results dict, printing incorrectly classified dogs
         for key in results_dic:
-
-            # TODO: 6c. REPLACE pass with CODE that prints out the pet label 
-            #           and the classifier label from results_dic dictionary    
-            #           ONLY when the classifier function (classifier label) 
-            #           misclassified dogs specifically: 
-            #             pet label is-a-dog and classifier label is-NOT-a-dog 
-            #               -OR- 
-            #             pet label is-NOT-a-dog and classifier label is-a-dog 
-            #          You will need to write a conditional statement that 
-            #          determines if the classifier function misclassified dogs
-            #          See 'Adjusting Results Dictionary' section in 
-            #         'Classifying Labels as Dogs' for details on the 
-            #          format of the results_dic dictionary. Remember the value
-            #          is accessed by results_dic[key] and the value is a list
-            #          so results_dic[key][idx] - where idx represents the 
-            #          index value of the list and can have values 0-4.
-            #
             # Pet Image Label is a Dog - Classified as NOT-A-DOG -OR- 
             # Pet Image Label is NOT-a-Dog - Classified as a-DOG
             if ((results_dic[key][3] == 1) and (results_dic[key][4] == 0)):
